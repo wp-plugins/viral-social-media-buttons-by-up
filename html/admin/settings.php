1010401ae05a8e13d4dbd4ed563bde548c2f56c0
upshare <upshare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 16:54:26 +0000

    Moved video embed JS
    git-svn-id: https://plugins.svn.wordpress.org/viral-social-media-buttons-by-up/trunk@1034586 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/admin/settings.php
+++ html/admin/settings.php
@@ -376,6 +376,58 @@
 	
 		ga('create', 'UA-47573483-1', 'auto');
 		ga('send', 'event', 'Widget Settings', 'Page View', 'WP Social Media Button Widget V3')
+		
+		
+		
+		var player = jQuery('#updemo');
+    var url = window.location.protocol + player.attr('src').split('?')[0];
+    var status = jQuery('.status');
+
+    // Listen for messages from the player
+    if (window.addEventListener){
+        window.addEventListener('message', onMessageReceived, false);
+    }
+    else {
+        window.attachEvent('onmessage', onMessageReceived, false);
+    }
+
+    // Handle messages received from the player
+    function onMessageReceived(e) {
+        var data = JSON.parse(e.data);
+        
+        switch (data.event) {
+            case 'ready':
+                onReady();
+                break;
+        }
+    }
+   
+    jQuery('#unmute').on('click', function() {
+        post('setVolume', "0.5");
+        jQuery(this).hide();
+        
+        ga('send', 'event', 'Widget Settings', 'Play Sound Click', 'WP Social Media Button Widget V3')
+    });
+
+
+    // Helper function for sending a message to the player
+    function post(action, value) {
+        var data = {
+          method: action
+        };
+        
+        if (value) {
+            data.value = value;
+        }
+        
+        var message = JSON.stringify(data);
+        player[0].contentWindow.postMessage(data, url);
+    }
+
+    function onReady() {       
+		post('play');
+		post('setVolume', "0");		
+    }
 	</script>
 	<?php
 	}
